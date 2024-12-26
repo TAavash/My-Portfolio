cd5cb44937551de2a581de0956efb358e2ce0ccd
AAVASH <aavashlopchan@gmail.com>
2024-12-26 22:25:18 +0545

    render once

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -19,18 +19,24 @@ const iconVariants = (duration) => ({
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <motion.h2 
-      whileInView={{opacity: 1, y: 0}}
-      initial={{opacity: 0, y: -100}}
-      transition={{duration: 1.5}}
-      viewport={{
-        once:true,
-      }}
-      className="my-20 text-center text-4xl">Technologies</motion.h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        viewport={{
+          once: true,
+        }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
       <motion.div
         whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: -100 }}
         transition={{ duration: 1.5 }}
+        viewport={{
+          once: true,
+        }}
         className="flex flex-wrap items-center justify-center gap-4"
       >
         <motion.div
