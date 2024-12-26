c166363ff2cb93be5f2377acefd900f23ea3604e
AAVASH <aavashlopchan@gmail.com>
2024-12-26 14:27:25 +0545

    Experience Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Technologies from "./components/Technologies";
@@ -20,6 +21,7 @@ const App = () => {
         <Hero />
         <About />
         <Technologies />
+        <Experience />
       </div>
     </div>
   );
