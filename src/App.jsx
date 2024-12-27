b4885a6c3e1c26ecbfc6a95d586eaf62dfa3e826
AAVASH <aavashlopchan@gmail.com>
2024-12-27 22:02:06 +0545

    theme

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,13 @@ import ThemeToggle from "./components/ThemeToggle"; // Import the ThemeToggle co
 
 const App = () => {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 dark:bg-neutral-950 light:bg-white">
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
+        {/* Main Background: Conditional for light/dark mode */}
         <div
-          className="absolute top-0 z-[-2] h-screen w-screen dark:bg-neutral-950 dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] light:bg-white light:bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"
+          className="absolute top-0 z-[-2] h-screen w-screen 
+            dark:bg-neutral-950 dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] 
+            light:bg-white light:bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"
         ></div>
       </div>
       <div className="container mx-auto px-8">
